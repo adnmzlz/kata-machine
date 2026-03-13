dd77dbf4833fb1f3e9382551df5fd80853244f57
adnmzlz <aidan.may12@gmail.com>
2026-03-13 18:22:43 +1100

    Crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+  let low: number = 0;
+  let high: number = breaks.length;
+
+  for (let i = 1; i <= Math.sqrt(high); i ++) {
+    if (breaks[i * i] === true) {
+      high = i * i;
+      break;
+    } 
+    low = i * i;
+  }
+
+  for (let j = low; j < high; j++) {
+    if (breaks[j] === true) {
+      return j;
+    }
+  }
+
+  return -1;
 }
