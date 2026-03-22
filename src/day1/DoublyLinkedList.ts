3eb463ed02a9237ce83ad3405d330194feafc4f2
adnmzlz <aidan.may12@gmail.com>
2026-03-22 15:10:46 +1100

    doubly linked list pt 2

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -34,7 +34,6 @@ export default class DoublyLinkedList<T> {
             throw new Error("too long johnny, index no exist");
         }
 
-        this.length++;
         if (idx === this.length) {
             this.append(item);
             return;
@@ -42,13 +41,9 @@ export default class DoublyLinkedList<T> {
             this.prepend(item);
             return;
         }
-
-        let curr = this.head;
-        for (let i = 0; curr && i < idx; i++) {
-            curr = curr.next;
-        }
-
-        curr = curr as Node<T>;
+        
+        this.length++;
+        const curr = this.getAt(idx) as Node<T>;
         const node = {value: item} as Node<T>;
 
         node.next = curr;
@@ -61,7 +56,6 @@ export default class DoublyLinkedList<T> {
     }
 
     append(item: T): void {
-
         this.length++;
         const node = {value: item} as Node<T>;
 
@@ -75,14 +69,68 @@ export default class DoublyLinkedList<T> {
 
         this.tail = node;
     }
-    
+
     remove(item: T): T | undefined {
+        let curr = this.head;
+        for (let i = 0; curr && i < this.length; i++) {
+            if (curr.value !== item) {
+                break;
+            }
+            curr = curr.next;
+        }
+        if (!curr) {
+            return undefined;
+        }
 
+        return this.removeNode(curr);
     }
+
     get(idx: number): T | undefined {
-
+        return this.getAt(idx)?.value;
     }
+
     removeAt(idx: number): T | undefined {
+        const node = this.getAt(idx);
+
+        if (!node) {
+            return undefined;
+        }
+
+        return this.removeNode(node);
+    }
+
+    private removeNode(node: Node<T>): T | undefined {
+        this.length--;
+
+        if (this.length === 0) {
+            const out = this.head?.value;
+            this.head = this.tail = undefined;
+            return out;
+        }
+
+        if (node.prev) {
+            node.prev = node.next;
+        }        
+        if (node.next) {
+            node.next = node.prev;
+        }
+        
+        if (node === this.head) {
+            this.head = node.next;
+        }
+        if (node === this.tail) {
+            this.tail = node.prev;
+        }
+        
+        node.prev = node.next = undefined;
+        return node.value;
+    }
+    private getAt(idx: number): Node<T> | undefined {
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next;
+        }
+        return curr;
 
     }
 }
