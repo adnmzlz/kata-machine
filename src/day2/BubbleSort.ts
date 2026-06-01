764fa7b0af573a5cfaeabe3a42440595ccabd7b7
adnmzlz <aidan.may12@gmail.com>
2026-06-01 21:28:22 +1000

    bubble day 2

--- src/day2/BubbleSort.ts
+++ src/day2/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
+  const x = arr.length - 1;
 
+  for (let i = 0; i <= x; i++) {
+    for (let j = 0; j < x - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
 }
