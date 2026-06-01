184b909b362472315fbc43bcf64519d3aa68f97f
adnmzlz <aidan.may12@gmail.com>
2026-06-01 21:23:49 +1000

    Quicksort day 2

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,18 +1,21 @@
 function qs(arr: number[], lo: number, hi: number): void {
+  // Base case: if lo index is bigger than hi index - essentially means (portion of?) the array is sorted
   if (lo >= hi) {
     return;
   }
 
   const pivotIdx = partition(arr, lo, hi);
 
-  qs(arr, lo, pivotIdx - 1);
-  qs(arr, pivotIdx + 1, hi);
+  qs(arr, lo, pivotIdx - 1); // Left side
+  qs(arr, pivotIdx + 1, hi); // Right side
 }
 
 function partition(arr: number[], lo: number, hi: number): number {
   const pivot = arr[hi];
 
   let idx = lo - 1;
+
+  // Walk from lo, to (but not including) hi (cos it's the pivot and we don't want to move that here)
 
   for (let i = lo; i < hi; i++) {
     if (arr[i] <= pivot) {
