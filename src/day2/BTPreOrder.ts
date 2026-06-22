958c03579007181cb971c34ef3e68ae4d211648a
adnmzlz <aidan.may12@gmail.com>
2026-06-22 22:34:38 +1000

    added checklist, did some BT traversals

--- src/day2/BTPreOrder.ts
+++ src/day2/BTPreOrder.ts
@@ -1,3 +1,14 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
+}
 
+function walk(head: BinaryNode<number> | null, path: number[]): number[] {
+  if (!head) return path;
+
+  path.push(head.value);
+
+  walk(head.left, path);
+  walk(head.right, path);
+
+  return path;
 }
